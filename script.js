9b40407bdaa7e15ca12c58ef94ed68aa426f9874
febriar1 <167492600+febriar1@users.noreply.github.com>
2024-05-26 04:46:44 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 // navbar
 const button = document.getElementById("hamburger");
 const navLinks = document.querySelector(".nav-link");
+const menuBtnIcon = document.querySelector("#hamburger i");
 
-button.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
+document.addEventListener("click", (e) => {
+  const isButtonClicked = button.contains(e.target);
+  const isNavLinkClicked = navLinks.contains(e.target);
+
+  if (isButtonClicked) {
+    navLinks.classList.toggle("open");
+    const open = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute(
+      "class",
+      isButtonClicked && open
+        ? "fa-solid fa-xmark"
+        : "fa-solid fa-bars-staggered"
+    );
+  } else if (!isButtonClicked && !isNavLinkClicked) {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "fa-solid fa-bars-staggered");
+  } else {
+    menuBtnIcon.setAttribute("class", "fa-solid fa-bars-staggered");
+  }
+});
+
+const nav = document.getElementById("navbar");
+const prevScroll = window.scrollY;
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+  const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
+  nav.style.top = currentScrollTop > lastScrollTop ? "-100px" : "0";
+  lastScrollTop = currentScrollTop;
 });
